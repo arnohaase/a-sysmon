e055963c90bd55525d9be4ced9b8f89b6d8a6f4a
Arno Haase <arno.haase@haase-consulting.com>
2013-12-11 13:31:09 +0100

    cleanup

---
+++ a-sysmon-core/src/main/java/com/ajjpj/asysmon/AShutdownServletContextListener.java
@@ -0,0 +1,23 @@
+package com.ajjpj.asysmon;
+
+import com.ajjpj.asysmon.config.AGlobalConfig;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+
+/**
+ * This listener takes care of shutting down the default A-SysMon instance on web application shutdown.
+ *
+ * @author arno
+ */
+public class AShutdownServletContextListener implements ServletContextListener {
+    @Override public void contextDestroyed(ServletContextEvent sce) {
+        if(AGlobalConfig.getImplicitlyShutDownWithServlet()) {
+            ASysMon.get().shutdown();
+        }
+    }
+
+    @Override public void contextInitialized(ServletContextEvent sce) {
+    }
+}
