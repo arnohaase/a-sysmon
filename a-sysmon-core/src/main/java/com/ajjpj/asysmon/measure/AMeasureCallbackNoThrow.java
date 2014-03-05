23c70e20890fb04509c2296be2285a39171ab64f
Arno Haase <arno.haase@haase-consulting.com>
2014-03-05 20:13:53 +0100

    cleanup

--- a-sysmon-core/src/main/java/com/ajjpj/asysmon/measure/AMeasureCallbackNoThrow.java
+++ a-sysmon-core/src/main/java/com/ajjpj/asysmon/measure/AMeasureCallbackNoThrow.java
@@ -4,4 +4,5 @@ package com.ajjpj.asysmon.measure;
  * @author arno
  */
 public interface AMeasureCallbackNoThrow<R> extends AMeasureCallback<R, RuntimeException> {
+    @Override R call(AWithParameters m);
 }
--- a-sysmon-core/src/main/java/com/ajjpj/asysmon/measure/AMeasureCallbackVoidNoThrow.java
+++ a-sysmon-core/src/main/java/com/ajjpj/asysmon/measure/AMeasureCallbackVoidNoThrow.java
@@ -4,4 +4,5 @@ package com.ajjpj.asysmon.measure;
  * @author arno
  */
 public interface AMeasureCallbackVoidNoThrow extends AMeasureCallbackVoid <RuntimeException> {
+    @Override void call(AWithParameters m);
 }
